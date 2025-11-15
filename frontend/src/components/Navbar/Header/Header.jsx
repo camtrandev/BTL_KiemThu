7b01148e4c8345b61a98dc86a2c40c4d6eead1a3
bacnguyen <bac03906@gmail.com>
2025-11-15 23:07:13 +0700

    push test

--- frontend/src/components/Navbar/Header/Header.jsx
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <h2>{t('Order Your Favorite Furnitures')}</h2>
         <img src={assets.salegif} alt="" className='salegif'/>
         <img src={assets.offer_gif} alt="" className='offergif' />
-        <p>{t('Choose from diverse menu fearturing a various array of Furniture craft with the excelent Artists around the world and finest ingredient which satisfy your demand and make your home decoration more luxurious, one better experience at a time.')}   </p>
+        <p>{t('Choose from diverse menu fearturing a various array of Furniture craft with the excelent Artists around the world and finest ingredient which satisfy your demand and make your home decoration more luxurious, one better experience at a time.')}</p>
         <button href='/#explore-menu' onClick={() => setActiveMenu('menu')} className={activeMenu === 'menu' ? 'active' : ''}>{t('View Menu')}</button>
       </div>
     </div>
